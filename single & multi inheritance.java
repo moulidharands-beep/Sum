e11d624d03255cbbf2e3cf11a7b18b308658192b
moulidharands-beep <moulidharands@gmail.com>
2025-08-28 10:25:18 +0530

    Update single & multi inheritance.java

--- single & multi inheritance.java
+++ single & multi inheritance.java
@@ -5,7 +5,6 @@ class Elephant {
     }
 }
 
-// First level derived class (Single Inheritance)
 class BabyElephant extends Elephant {
     void play() {
         System.out.println("Baby elephant is playing.");
@@ -19,24 +18,20 @@ class BabyAfricanElephant extends BabyElephant {
     }
 }
 
-// Main class to run the program
 public class Main {
     public static void main(String[] args) {
-        // Creating object of base class
         Elephant e = new Elephant();
         e.eat();
         System.out.println();
-
-        // Creating object of first derived class (Single Inheritance)
         BabyElephant be = new BabyElephant();
-        be.eat();   // Inherited from Elephant
-        be.play();  // Own method
+        be.eat();   
+        be.play();  
         System.out.println();
 
-        // Creating object of second derived class (Multilevel Inheritance)
-        BabyAfricanElephant bae = new BabyAfricanElephant();
-        bae.eat();       // From Elephant
-        bae.play();      // From BabyElephant
-        bae.trumpet();   // Own method
+        
+        bae.eat();      
+        bae.play();    
+        bae.trumpet();   
     }
 }
+
